35840398905a61eaadd6c902539933f747b95cb2
Eman <emmanuoel.h@gmail.com>
2015-11-22 16:59:25 -0800

    Fix bug that prevents enemy appearance

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ var Enemy = function (x, y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 'x';
-    this.y = 'y';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
